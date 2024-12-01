c9fc7e254adeca02112e3ccb3587be54e7f583fb
SahilLaptop <sahilvishwakarma365@gmail.com>
2024-12-01 11:50:38 +0530

    changing page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 
+  
 export default function Home() {
   return (
     <div className="container">
       <h1>Welcome to Next.js!</h1>
+      <h2>my first next js app</h2>
     
     </div>
   );
